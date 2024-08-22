1f10b904fcead01311c86a7b86f94cde206418b0
M-Farooq-Azaad <mr.farooq2066@gmail.com>
2024-08-22 13:40:30 +0500

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
+import ErrorPage from "./components/ErrorPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <span className="hidden sm:block">
-          <Tech />
-        </span>
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <div className='relative z-0 bg-primary'>
+            <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+              <Navbar />
+              <Hero />
+            </div>
+            <About />
+            <Experience />
+            <span className="hidden sm:block">
+              <Tech />
+            </span>
+            <Works />
+            <Feedbacks />
+            <div className='relative z-0'>
+              <Contact />
+              <StarsCanvas />
+            </div>
+          </div>
+        } />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
